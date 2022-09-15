afe8159f51ce63b1fb18253b3ff6b366788b8f17
Javier Ochoa <jochoa@redhat.com>
2022-09-15 16:04:53 -0400

    Minor changes

--- src/main/java/com/rhsso/websecurity/WebsecurityApplication.java
+++ src/main/java/com/rhsso/websecurity/WebsecurityApplication.java
@@ -1,4 +1,4 @@
-package com.codeusingjava.websecurity;
+package com.rhsso.websecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/rhsso/websecurity/config/WebSecurityConfiguration.java
+++ src/main/java/com/rhsso/websecurity/config/WebSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.codeusingjava.websecurity.config;
+package com.rhsso.websecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
