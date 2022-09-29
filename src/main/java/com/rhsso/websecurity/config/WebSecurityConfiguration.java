679c1b109f1b78556b838c321b2652ae6a63d95c
mikheladun <madun@redhat.com>
2022-09-29 14:57:39 -0400

    removed WebSecurityConfigurerAdapter implementation

--- src/main/java/com/rhsso/websecurity/config/WebSecurityConfiguration.java
+++ src/main/java/com/rhsso/websecurity/config/WebSecurityConfiguration.java
@@ -1,25 +1,17 @@
 package com.rhsso.websecurity.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration {
 
     private final KeycloakLogoutHandler keycloakLogoutHandler;
 
@@ -32,18 +24,13 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/h2-console").permitAll()
                 .antMatchers("/api/*").hasAnyRole("USER","ADMIN")
                 .anyRequest().authenticated()
                 .and().logout().permitAll();
-                // .permitAll();//.authenticated();
-                // .and()
-                // .formLogin().permitAll()
-                // .and()
-                // .logout().permitAll();
 
         http.oauth2Login()
                 .and()
@@ -54,28 +41,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.headers().frameOptions().sameOrigin();
 
+        return http.build();
     }
 
-
-//    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return (DelegatingPasswordEncoder) PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception{
-       return  super.authenticationManagerBean();
-    }
-
- @Autowired
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication()
-		.dataSource(dataSource).passwordEncoder(passwordEncoder())
-		.usersByUsernameQuery("select username,password,enabled from UsersTable where username=?")
-		.authoritiesByUsernameQuery("select username,authority from authorities where username=?");
-	}
 }
